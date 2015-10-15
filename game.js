0bbdba1ee6bbe7975b1877b4626d819e78746d60
ea7890 <ericrobertalvarez@gmail.com>
2015-10-15 15:57:24 +1100

    Tiger is working :)

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 
-        var clickedTime; //time when user clicks on box
-        var createdTime; // time when box is appears   
-        var reactionTime; // seconds that pass between when the box is created and when it is clicked on
+var clickedTime; //time when user clicks on box
+var createdTime; // time when box is appears   
+var reactionTime; // seconds that pass between when the box is created and when it is clicked on
 
 
 
 
-    function makeBox() {
+function makeBox() {
 
-        var time = Math.random(); //random value between 0 and 1
-        time = time * 5000;       //changes var time to a random value between 0 and 5000
+    var time = Math.random(); //random value between 0 and 1
+    time = time * 5000;       //changes var time to a random value between 0 and 5000
 
-        setTimeout (function() {  //box appears after a certain amount of time
+    setTimeout (function() {  //box appears after a certain amount of time
         document.getElementById("box").style.display="block"; //changing display:none to display:block makes the box appear
 
         createdTime = Date.now();
@@ -25,23 +25,72 @@
         document.getElementById("box").style.left = left + "px"; 
         ///////Make shape appear in random locations////////////////
 
-        }, time); 
+    }, time); 
 
+}
 
-    }
+// function that makes the square disappear when the user clicks on it.
+document.getElementById("box").onclick=function(){
+    document.getElementById("box").style.display="none";
 
-        // function that makes the square disappear when the user clicks on it.
-        document.getElementById("box").onclick=function(){
-        document.getElementById("box").style.display="none";
+    clickedTime = Date.now();
+    reactionTime =  (clickedTime - createdTime)/1000;
 
-        clickedTime = Date.now();
-        reactionTime =  (clickedTime - createdTime)/1000;
-
-        document.getElementById("yourTime").innerHTML=reactionTime; // prints the time between when the box was created and when it was clicked, by changing the innerHTML    
-
-        
-        makeBox(); // the box will reset and reappear once it is clicked.
-        } 
+    document.getElementById("yourTime").innerHTML=reactionTime; // prints the time between when the box was created and when it was clicked, by changing the innerHTML    
 
         
-        makeBox();
+    makeBox(); // the box will reset and reappear once it is clicked.
+}
+
+
+makeBox();
+
+//////////////Make tiger////////////////////////
+
+var makeTiger = function(){
+    var tigerTime = Math.random();
+    tigerTime=tigerTime*10000; //tiger appears at random time
+
+    setTimeout(function() {
+    
+        document.getElementById("box1").style.display="block";
+
+        var top = Math.random() * 500;
+        var left = Math.random()* 1000;
+
+        document.getElementById("box1").style.top = top + "px";
+        document.getElementById("box1").style.left = left + "px";
+
+        hideTiger();
+    }, tigerTime);
+     
+
+}
+
+document.getElementById("box1").onclick=function() {
+    document.getElementById("box1").style.display="none";
+    alert("Game Over! You got eaten by the Evil Tiger")
+}
+
+var hideTiger = function () {
+    setTimeout(function() {
+    
+        document.getElementById("box1").style.display="none";
+         makeTiger();
+    }, 10000);
+   
+}
+
+makeTiger();
+
+
+
+
+
+
+
+
+
+
+
+
